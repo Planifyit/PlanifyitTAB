4e33d0a0f5a0b679c84b6793420dc18d5db58466
planifyit.com <contact@planifyit.com>
2025-03-28 16:50:56 +0100

    Update planifyit-tab-widget.js

--- planifyit-tab-widget.js
+++ planifyit-tab-widget.js
@@ -410,7 +410,7 @@ _handleSelectAll(e) {
         detail: {
             properties: {
                 selectedRows: JSON.stringify(this._selectedRows),
-              
+          selectedRowsArray: this._selectedRows,     
          selectedRowsData: JSON.stringify(this._selectedRowsData)
        
             }
@@ -434,6 +434,7 @@ _handleRowClick(index, e) {
                 properties: {
                 
                     selectedRows: JSON.stringify(this._selectedRows),
+                       selectedRowsArray: this._selectedRows,  
                     selectedRowsData: JSON.stringify(this._selectedRowsData)
 
                 
@@ -461,6 +462,7 @@ _handleCheckboxChange(index, e) {
         detail: {
             properties: {
                 selectedRows: JSON.stringify(this._selectedRows),
+                   selectedRowsArray: this._selectedRows,  
     selectedRowsData: JSON.stringify(this._selectedRowsData)
        
             }
